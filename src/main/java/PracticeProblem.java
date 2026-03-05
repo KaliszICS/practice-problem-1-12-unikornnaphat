07d67bc79789a29132ad70eb6a46280c8f3a5f57
unikornnaphat <350364092@tdsb.ca>
2026-03-05 16:01:13 +0000

    1.12 practice problem-sandy

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -8,19 +10,43 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Input an integer: ");
+        int num = scanner.nextInt();
+        int result = num + 3;
+        System.out.println(result);
+        scanner.close();
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        String numStr = scanner.next();
+        String concatenated = numStr + "4";
+        double num = Double.parseDouble(concatenated);
+        double result = num + 2;
+        System.out.println(result);
+        scanner.close();
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a radius: ");
+        double radius = scanner.nextDouble();
+        double pi = 3.14;
+        double area = pi * radius * radius;
+        System.out.println(area);
+        scanner.close();
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        double num = scanner.nextDouble();
+        double multiplied = num * 12;
+        long result = (long) Math.floor(multiplied);
+        System.out.println(result);
+        scanner.close();
 	}
 
 }
